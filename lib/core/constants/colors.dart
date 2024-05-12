fb8d9771e4d73e32360e8581baa48562e0096681
zahidayturan <21060607@stu.omu.edu.tr>
2024-05-12 22:35:13 +0300

    directory and colors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  Color greenDark =  const Color(0xFF10403B);
+  Color green =  const Color(0xFF127369);
+  Color blue = const Color(0xFF038C8C);
+  Color blueLight = const Color(0xFF8AA6A3);
+  Color greyDark = const Color(0xFF4C5958);
+  Color grey = const Color(0xFFF5F5F5);
+  Color white = const Color(0xFFFFFFFF);
+  Color orange = const Color(0xFFF24405);
+  Color red = const Color(0xFFD93250);
+  Color yellow = const Color(0xFFF2B705);
+  Color lemon = const Color(0xFFAFBF36);
+  Color black = const Color(0xFF000000);
+}

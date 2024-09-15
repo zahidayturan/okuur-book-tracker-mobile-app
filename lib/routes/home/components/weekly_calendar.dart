9f9ed0be9a40bc03aade972b5e8ae3383bf287fc
zahidayturan <21060607@stu.omu.edu.tr>
2024-09-15 22:22:38 +0300

    new  mini home design

--- lib/routes/home/components/weekly_calendar.dart
+++ lib/routes/home/components/weekly_calendar.dart
@@ -17,15 +17,8 @@ class WeeklyCalendar extends StatefulWidget {
 class _WeeklyCalendarState extends State<WeeklyCalendar> {
 
   AppColors colors = AppColors();
-  List<DateTime> weeks = [
-    DateTime.now().subtract(Duration(days: 3)),
-    DateTime.now().subtract(Duration(days: 2)),
-    DateTime.now().subtract(Duration(days: 1)),
-    DateTime.now(),
-    DateTime.now().add(Duration(days: 1)),
-    DateTime.now().add(Duration(days: 2)),
-    DateTime.now().add(Duration(days: 3)),
-  ];
+
+  DateTime initDate = DateTime.now();
 
   List<String> months = [
     "",
@@ -48,144 +41,101 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
   }
 
   Widget weekdays(){
-    return Container(
-      height: 50,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          arrowContainer(0),
-          dayContainer(28,28, weeks[0], colors.greenDark, 12,"FontMedium",true),
-          dayContainer(32,32, weeks[1], colors.greenDark, 13,"FontMedium",true),
-          dayContainer(36,36, weeks[2], colors.greenDark, 14,"FontMedium",true),
-          dayContainer(46,66, weeks[3], colors.green, 15,"FontBold",false),
-          dayContainer(36,36, weeks[4], colors.greyDark, 14,"FontMedium",true),
-          dayContainer(32,32, weeks[5], colors.greyDark, 13,"FontMedium",true),
-          dayContainer(28,28, weeks[6], colors.greyDark, 12,"FontMedium",true),
-          arrowContainer(1)
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        arrowContainer(0),
+        const SizedBox(width: 8,),
+        dayContainer(initDate),
+        const SizedBox(width: 8,),
+        arrowContainer(1)
+      ],
     );
   }
 
-  Widget dayContainer(double height,double width,DateTime date,Color color,double fontSize,String family,bool isCircle){
+  Widget dayContainer(DateTime date) {
     String dayInfo = "";
     bool isSameDay(DateTime date1, DateTime date2) {
       return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
     }
-    dayInfo = isSameDay(date, DateTime.now()) ? "Bugün" : months[date.month];
+    dayInfo = isSameDay(date, DateTime.now()) ? "Bugün" : "${date.year}";
 
-    int listIndex = weeks.indexWhere((element) => element == date);
     return GestureDetector(
-      onTap: () {
-        setState(() {
-          if(listIndex == 0){
-            weeks = weeks.map((date) => date.subtract(Duration(days: listIndex+3))).toList();
-          }else if(listIndex==1){
-            weeks = weeks.map((date) => date.subtract(Duration(days: listIndex+1))).toList();
-          }else if(listIndex==2){
-            weeks = weeks.map((date) => date.subtract(Duration(days: listIndex-1))).toList();
-          }else if(listIndex>=4){
-            weeks = weeks.map((date) => date.add(Duration(days: listIndex-3))).toList();
-          }else{
-
-          }
-
-        });
+      onTap: () async {
+        DateTime? selectedDate = await showDatePicker(
+          context: context,
+          initialDate: initDate,
+          firstDate: DateTime(2000),
+          lastDate: DateTime(2100),
+        );
+        if (selectedDate != null && selectedDate != initDate) {
+          setState(() {
+            initDate = selectedDate;
+          });
+        }
       },
       onLongPress: () {
         setState(() {
-          if(listIndex == 3){
-            weeks = [
-              DateTime.now().subtract(Duration(days: 3)),
-              DateTime.now().subtract(Duration(days: 2)),
-              DateTime.now().subtract(Duration(days: 1)),
-              DateTime.now(),
-              DateTime.now().add(Duration(days: 1)),
-              DateTime.now().add(Duration(days: 2)),
-              DateTime.now().add(Duration(days: 3)),
-            ].toList();
-          }
+          initDate = DateTime.now();
         });
       },
       child: Container(
-        width: width,
-        height: height,
+        constraints: const BoxConstraints(minWidth: 160),
         decoration: BoxDecoration(
-          borderRadius: isCircle == true ? 
-          BorderRadius.all(Radius.circular(30)) :
-            BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30),bottomLeft: Radius.circular(18),bottomRight: Radius.circular(18)),
-          color: color
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(6),
+              topRight: Radius.circular(6),
+              bottomLeft: Radius.circular(14),
+              bottomRight: Radius.circular(14)),
+          color: colors.green,
         ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              AnimatedSwitcher(
-                duration: Duration(milliseconds: 300),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return FadeTransition(
-                    opacity: animation,
-                    child: child,
-                  );
-                },
-                child: Text(
-                  date.day.toString(),
-                  key: ValueKey<int>(date.day),
-                  style: TextStyle(
-                    color: colors.white,
-                    fontSize: fontSize,
-                    fontFamily: family,
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: width != height,
-                child: AnimatedSwitcher(
-                  duration: Duration(milliseconds: 400),
-                  transitionBuilder: (Widget child, Animation<double> animation) {
-                    return FadeTransition(
-                      opacity: animation,
-                      child: child,
-                    );
-                  },
-                  child: Text(
-                    dayInfo,
-                    key: ValueKey<String>(dayInfo),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: colors.white,
-                      fontSize: 10,
-                      fontFamily: "FontMedium",
-                    ),
-                  ),
-                ),
-              ),
-            ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            },
+            child: RichTextWidget(
+              texts: ["${date.day.toString()} ${months[date.month]} ", dayInfo],
+              colors: [colors.white, colors.white],
+              fontFamilies: ["FontBold", "FontMedium"],
+              fontSize: 14,
+              key: ValueKey<int>(date.day),
+              align: TextAlign.center,
+            ),
           ),
-        )
+        ),
       ),
     );
   }
+
 
   GestureDetector arrowContainer(int type) {
     return GestureDetector(
       onTap: () {
         setState(() {
           type == 0 ?
-          weeks = weeks.map((date) => date.subtract(Duration(days: 1))).toList()
+          initDate = initDate.subtract(const Duration(days: 1))
               :
-          weeks = weeks.map((date) => date.add(Duration(days: 1))).toList();
+          initDate = initDate.add(const Duration(days: 1));
         });
       },
       child: Container(
-        height: 54,
-        width: 18,
-        padding: EdgeInsets.all(4),
+        height: 28,
+        width: 28,
+        decoration: BoxDecoration(
+          color: colors.grey,
+          shape: BoxShape.circle
+        ),
+        padding: const EdgeInsets.all(6),
         child: RotatedBox(
             quarterTurns: type == 0 ? 2 : 0,
-            child: Image.asset("assets/icons/arrow.png", color: colors.blueLight)
+            child: Image.asset("assets/icons/arrow.png",color: colors.green)
         ),
       ),
     );
@@ -197,16 +147,16 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
           borderRadius: const BorderRadius.all(Radius.circular(16)),
           color: colors.white
       ),
-      padding: EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           weekdays(),
-          SizedBox(height: 4,),
+          const SizedBox(height: 8,),
           title("Günlük Hedefe Ulaşıldı!", colors.green, 14, "FontMedium"),
-          SizedBox(height: 4,),
+          const SizedBox(height: 4,),
           title("Kralın Dönüşü kitabından", colors.black, 13, "FontMedium"),
-          SizedBox(height: 8,),
+          const SizedBox(height: 8,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -214,7 +164,7 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
             iconAndText("assets/icons/clock.png", "dakika","54"),
             iconAndText("assets/icons/point.png", "puan+","40"),
           ],),
-          SizedBox(height: 8,),
+          const SizedBox(height: 8,),
           RichTextWidget(
               texts: ["Bu ","haftanın en iyi okumasını"," yaptın."],
               colors: [colors.black,colors.black,colors.black],
@@ -248,10 +198,10 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
             shape: BoxShape.circle,
             color: colors.blueLight
           ),
-          padding: EdgeInsets.all(5),
+          padding: const EdgeInsets.all(5),
           child: Image.asset(path,color: colors.white,),
         ),
-        SizedBox(width: 4,),
+        const SizedBox(width: 4,),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -262,6 +212,5 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
       ],
     );
   }
-
 
 }
